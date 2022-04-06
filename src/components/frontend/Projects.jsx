f5dc8235e6dc6000c8b9d3cfcfb0e4ea5d6db7ab
xanax <34755589+anKordii@users.noreply.github.com>
2022-04-06 17:39:24 +0200

    Update Projects.jsx

--- src/components/frontend/Projects.jsx
+++ src/components/frontend/Projects.jsx
@@ -41,7 +41,7 @@ function Projects() {
 
               <div className={`${'col-md-6 p-5 mb-5'} ${style.spotted__bg}`}>
                 <div className={` ${'text-center'} ${style.spotted__header}`}>
-                  <img src="http://www.zsz4.opole.pl/templates/jsn_metro_pro/images/colors/white/logo.png" alt="avatar" className="img-fluid"/>
+                  <img src="https://cdn.beyondlabs.pl/spotted/logo.png" alt="avatar" className="img-fluid"/>
                   <div className={`${'p-3 mb-3'} ${style.spotted__info}`}>
                     <h3>Kotleciarnia Opole</h3>
                     <p style={{marginBottom: '0'}}>
