15a8583deaaa95de1346c937110d3f1c4d155eb5
xanax <34755589+anKordii@users.noreply.github.com>
2022-04-06 22:13:48 +0200

    Socket:)

--- src/components/frontend/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Navigation } from "./Navigation";
-export { default as Projects } from "./Projects";
-export { default as Footer } from "./Footer";
-export { default as Privacy } from "./Privacy";
-export { default as BackTop } from "./BackTop";
